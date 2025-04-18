6956e3f566eae11792fcf606059a56d9e7b6b047
VityaAresov <158539713+VityaAresov@users.noreply.github.com>
2025-04-18 00:46:24 -0700

    Update layout.tsx

--- viks_media-main/src/app/layout.tsx
+++ viks_media-main/src/app/layout.tsx
@@ -1,3 +1,4 @@
+// src/app/layout.tsx
 import './global.css'
 import Header from '@/components/common/Header'
 import Footer from '@/components/common/Footer'
@@ -5,28 +6,30 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <head />
       <body className={inter.className}>
         <Header />
 
-        {/* 
-          Вот наша новая обёртка, которая масштабирует только контент 
-          (хедер и футер остаются нетронутыми)
-        */}
         <div
           style={{
-            zoom: 0.9;
+            transform: 'scale(0.9)',
+            transformOrigin: 'top left',
+            width: '111.1111%',
+            margin: '0 auto',
           }}
         >
           <main className="flex-grow container mx-auto px-6 py-8">
             {children}
           </main>
+          <Footer />
         </div>
-
-        <Footer />
       </body>
     </html>
   )
