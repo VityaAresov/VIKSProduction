3c9ca02536aa3a8c7d6203e2414aa1bf22358d49
VityaAresov <158539713+VityaAresov@users.noreply.github.com>
2025-07-31 23:29:52 -0600

    Create page.tsx

---
+++ viks_media-main/src/app/services/page.tsx
@@ -0,0 +1,9 @@
+// /src/app/services/page.tsx
+
+'use client';
+
+import ServicesPage from '@/components/Service/ServicesPage';
+
+export default function Services() {
+  return <ServicesPage />;
+}
